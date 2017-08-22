dbeca61710ab5fe93bdd2c30cf24c4b9c4ca5369
Igor Vieira <igor.p.r.vieira@gmail.com>
2017-08-22 14:04:08 -0300

    initial commit

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul v-for="project in listProjects" :key="project.id">
+      <li>
+        {{project.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      listProjects: [{id: 1, name: 'Hey'}]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
